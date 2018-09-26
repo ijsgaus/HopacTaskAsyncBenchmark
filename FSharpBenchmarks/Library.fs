7cec13ad942075c4c841b3d49253f4f6399bca25
Vladimir Shchur <shchurvladimir@gmail.com>
2018-09-26 21:24:15 +0300

    Improved fsharp methods

--- FSharpBenchmarks/Library.fs
+++ FSharpBenchmarks/Library.fs
@@ -6,7 +6,7 @@ open System.Threading.Tasks
 open System
 open FSharp.Control.Tasks.V2.ContextInsensitive
 
-let reduceParallelAsync<'a> f (ie: 'a array) =
+let reduceParallelAsync f (ie: 'a array) =
     let rec reduce (ie : 'a array, f, start, finish) =
         async {
             match (finish - start + 1) with
@@ -16,15 +16,17 @@ let reduceParallelAsync<'a> f (ie: 'a array) =
                 return f ie.[start] ie.[start+1]
             | len ->
                 let h = len / 2 + start
-                let! o1 = reduce(ie, f, start, h-1)
-                let! o2 = reduce(ie, f, h, finish)
+                let o1Task = reduce(ie, f, start, h-1)
+                let o2Task = reduce(ie, f, h, finish)
+                let! o1 = o1Task
+                let! o2 = o2Task
                 return f o1 o2
         }
     match ie.Length with
     | 0 -> failwith "Sequence contains no elements"
     | _ -> (reduce (ie, f, 0, ie.Length-1)) |> Async.RunSynchronously
 
-let reduceParallelTaskBuilder<'a> f (ie: 'a array) =
+let reduceParallelTaskBuilder f (ie: 'a array) =
     let rec reduce (ie : 'a array, f, start, finish) =
         task {
             match (finish - start + 1) with
@@ -34,15 +36,17 @@ let reduceParallelTaskBuilder<'a> f (ie: 'a array) =
                 return f ie.[start] ie.[start+1]
             | len ->
                 let h = len / 2 + start
-                let! o1 = reduce(ie, f, start, h-1)
-                let! o2 = reduce(ie, f, h, finish)
+                let o1Task = reduce(ie, f, start, h-1)
+                let o2Task = reduce(ie, f, h, finish)
+                let! o1 = o1Task
+                let! o2 = o2Task
                 return f o1 o2
         }
     match ie.Length with
     | 0 -> failwith "Sequence contains no elements"
     | _ -> (reduce (ie, f, 0, ie.Length-1)).Result
 
-let reduceFsharpSync<'a> f (ie: 'a array) =
+let reduceFsharpSync f (ie: 'a array) =
     let rec reduce (ie : 'a array, f, start, finish) =
         match (finish - start + 1) with
         | 1 -> ie.[start]
@@ -56,15 +60,14 @@ let reduceFsharpSync<'a> f (ie: 'a array) =
     | 0 -> failwith "Sequence contains no elements"
     | _ -> reduce (ie, f, 0, ie.Length-1)
 
-let reduceParallelHopac<'a> f (a: 'a array) =
-    let rec reduceRec f (start, finish) =
-        let reduce = reduceRec f
+let reduceParallelHopac f (a: 'a array) =
+    let rec reduce (a: 'a array, f, start, finish) =
         match (finish - start + 1) with
         | 1 -> Job.result a.[start]
         | 2 -> Job.result (f a.[start] a.[start+1])
         | len ->
             let h = len / 2 + start
-            reduce (start, h-1) <*> Job.delayWith reduce (h, finish) >>- fun (x, y) -> f x y
+            reduce (a, f, start, h-1) <*> Job.delayWith reduce (a, f, h, finish) >>- fun (x, y) -> f x y
     match a.Length with
     | 0 -> failwith "Sequence contains no elements"
-    | _ -> run <| reduceRec f (0, a.Length-1)
+    | _ -> run <| reduce (a, f , 0, a.Length-1)
